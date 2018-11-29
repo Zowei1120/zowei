8e30ae861d86ec6312fde96b6872fff7057c55ba
Zowei1120 <45396448+Zowei1120@users.noreply.github.com>
2018-11-29 20:26:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, message)
 
 def Reply(text):
-    if text == "hi":
+    if text == "hi"OR"Hi":
         text='hello'
         return text
     else:
