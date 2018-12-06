3d7330dd043614675d212f9805107b4a3e5a3da5
Zowei1120 <45396448+Zowei1120@users.noreply.github.com>
2018-12-06 19:19:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def Button(event):
     message = TemplateSendMessage(
         alt_text='Buttons template',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://example.com/image.jpg',
+            thumbnail_image_url='水豚.jpg',
             title='Menu',
             text='Please select',
             actions=[
