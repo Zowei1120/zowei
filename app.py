5fb0cf43658dc5ea26aebc0c396172d225714c4a
Zowei1120 <45396448+Zowei1120@users.noreply.github.com>
2018-12-20 19:32:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,34 @@ def callback():
 
 #----------從這裡開始複製----------
 
+#會員系統
+def GetUserlist():
+    userlist = {}
+    file = open('users','r')
+    while True :
+        temp = file.readline().strip().split(',')
+        if temp[0] == "" : break
+        userlist[temp[0]] = temp[1]
+    file.close()
+    return userlist
+
+#登入系統
+def Login(event,userlist):
+    i = 0
+    for user in userlist.keys():
+        if event.source.user_id == user:
+            return i
+        i+=1
+    return -1
+
+#寫入資料
+def Update(userlist):
+    file = open('users','w')
+    for user in userlist.keys():
+        file.write(user+','+userlist[user])
+    file.close()
+
+
 #關鍵字系統
 def Keyword(event):
     KeyWordDict = {"你好":["text","你好你好"],
@@ -88,6 +116,27 @@ def Command(event):
         return True
     else:
         return False
+    
+#新增一個參數
+def Reply(event,userlist):
+    if not Command(event):
+        Ktemp = KeyWord(event)
+        if Ktemp[0]:
+            line_bot_api.reply_message(event.reply_token,
+                TextSendMessage(text = Ktemp[1]))
+        else:
+            if userlist[event.source.user_id] == '-1':
+                line_bot_api.reply_message(event.reply_token,
+                    TextSendMessage(text = "你知道台灣最稀有、最浪漫的鳥是哪一種鳥嗎？"))
+                userlist[event.source.user_id] = '0'
+            else:
+                if event.message.text == "黑面琵鷺":
+                    line_bot_api.reply_message(event.reply_token,
+                        TextSendMessage(text = "你居然知道答案!!!"))
+                else:
+                    line_bot_api.reply_message(event.reply_token,
+                        TextSendMessage(text = "答案是：黑面琵鷺!!!因為每年冬天，他們都會到台灣來\"壁咚\""))
+                userlist[event.source.user_id] = '-1'
 
 #回覆函式，指令 > 關鍵字 > 按鈕
 def Reply(event):
@@ -99,15 +148,23 @@ def Reply(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-        Reply(event)
-        '''
-        line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.source.user_id + "說:"))
-        line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.message.text))
-        '''
+        userlist = GetUserlist()
+        clientindex = Login(event,userlist)
+        if clientindex > -1:
+            Reply(event,userlist)
+            '''
+            line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.source.user_id + "說:"))
+            line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.message.text))
+            '''
+        else:
+            userlist[event.source.user_id] = '-1';
+            line_bot_api.reply_message(event.reply_token, 
+                TextSendMessage(text="註冊成功"))
+        Update(userlist)
     except Exception as e:
         line_bot_api.reply_message(event.reply_token, 
             TextSendMessage(text=str(e)))
-
+        
 #處理Postback
 @handler.add(PostbackEvent)
 def handle_postback(event):
